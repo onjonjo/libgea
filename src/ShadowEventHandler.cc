355abf36df4ada0e178f5c5f5ff9264370bbc60e
Timo Lindhorst <TimoLindhorst@web.de>
2008-04-09 18:52:13 +0200

    Fix incorrect deletion of events.
    When an event is fired by a timeout during the execution a new event may be
    added to the event list. If this new event has a timeout that lies in the past,
    the event is added to the front of the list. So after the execution it must not
    be the _first_ element that is deleted, but the _former first_ element.

--- src/ShadowEventHandler.cc
+++ src/ShadowEventHandler.cc
@@ -270,16 +270,19 @@ DLLEXPORT void gea::ShadowEventHandler::run() {
 	}
 	if (sel_ret == 0) { // timeout
 	    // must be the first one..
+	    EventList::iterator evIter = eventList.begin();
 
-	    eventList.begin()->second.h->status = Handle::Timeout;
+	    evIter->second.h->status = Handle::Timeout;
 
-	    this->masterEventHandler->lastEventTime = eventList.begin()->first; // store event time for later use
+	    // store event time for later use, but only increase value
+	    if (evIter->first > this->masterEventHandler->lastEventTime)
+		    this->masterEventHandler->lastEventTime = evIter->first;
 
-	    eventList.begin()->second.e(eventList.begin()->second.h,
-					eventList.begin()->first,
-					eventList.begin()->second.data);
+	    evIter->second.e(evIter->second.h,
+			     this->masterEventHandler->lastEventTime,
+			     evIter->second.data);
 
-	    eventList.erase(eventList.begin());
+	    eventList.erase(evIter);
 
 
 	} else {
